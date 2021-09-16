33396a6b195cb262615d591cecd83e380eba9882
michihiro <pomeofishbird0303@gmail.com>
2021-09-16 23:44:36 +0900

    productLisiのuiを調整

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -3,6 +3,28 @@ import { useQuery } from '@apollo/react-hooks';
 import { Card, Button, ResourceList, Stack, TextStyle, Thumbnail } from '@shopify/polaris';
 import store from 'store-js';
 
+interface Product {
+  title: string;
+  handle: string;
+  id: string;
+  images: {
+    edges: {
+      node: {
+        originalSrc: string;
+        altText: string;
+      }
+    }
+  }
+  variants: {
+    edges: {
+      node: {
+        price: number;
+        id: string;
+      }
+    }
+  }
+ }
+
 const GET_PRODUCTS_BY_ID = gql`
   query getProducts($ids: [ID!]!) {
     nodes(ids: $ids) {
@@ -40,12 +62,50 @@ function ProductList() {
 
   return (
     <div>
-      <h1>Hello</h1>
-      {data.nodes.map(item => {
-        return (
-          <p>{item.title}</p>
+
+          <Card>
+            <ResourceList
+              showHeader
+              resourceName={{ singular: "Product", plural: "Products" }}
+              items={data.nodes}
+              renderItem={(item: Product) => {
+                const media = (
+                 <Thumbnail
+                    source={
+                      item.images.edges[0]  ? item.images.edges[0].node.originalSrc : ""
+                    }
+                    alt={item.images.edges[0] ? item.images.edges[0].altText : ""}
+                  />
+                )
+                const price = item.variants.edges[0].node.price
+                return (
+                    // @ts-ignore
+                  <ResourceList.Item
+                    id={item.id}
+                    media={media}
+                    accessibilityLabel={`View details for ${item.title}`}
+                  >
+                  <Stack>
+                      <Stack.Item fill>
+                        <h3>
+                        <TextStyle variation="strong">
+                           {item.title}
+                        </TextStyle>
+                        </h3>
+
+                      </Stack.Item>
+                      <Stack.Item>
+                        <p>${price}</p>
+                      </Stack.Item>
+                      </Stack>
+                      </ResourceList.Item>
+
+                )
+              }}
+            />
+          </Card>
         )
-      })}
+
     </div>
   )
 }
